17cf8f65b3a305a8805d317af607c056308b0791
Sufyan Menk <smenk@hawk.iit.edu>
2018-01-28 23:21:25 -0600

    Added initial age checker

--- pp-01/site.js
+++ pp-01/site.js
@@ -25,5 +25,21 @@ $("#contact-form").on("submit",
                 $("#phone-section").append("<li class='error'>Invalid phone number.</li>");
             }
         }
+    
+        //Validate Age
+        var current = new Date();
+        var dob = new Date($("#birthday").val());
+        if(current.getFullYear - dob.getFullYear > 18){
+            valid.push(1);
+        }
+        else if(current.getFullYear - dob.getFullYear == 18){
+            
+        }
+        else{
+            valid.push(0);
+            if(!$("#birthday-section").find("li").hasClass("error")){
+                $("#birthday-section").append("<li class='error'>Sorry you are under 18.</li>");
+            }
+        }
     }
 );
