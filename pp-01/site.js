0c028c551e3035371a873eb65a23f2b9e35e8479
Sufyan Menk <smenk@hawk.iit.edu>
2018-01-28 23:14:01 -0600

    Added phone validation and check if error already exists so it doesnt appear again

--- pp-01/site.js
+++ pp-01/site.js
@@ -9,7 +9,21 @@ $("#contact-form").on("submit",
         }
         else {
             valid.push(0);
-            $("#email-section").append("<li class='error'>Invalid email address.</li>");
+            if(!$("#email-section").find("li").hasClass("error")){
+                $("#email-section").append("<li class='error'>Invalid email address.</li>");
+            }
+        }
+    
+        //Validate Phone
+        var phone = $("#phone").val();
+        if(/.d{10}/.test(phone)){
+           valid.push(1); 
+        }
+        else {
+            valid.push(0);
+            if(!$("#phone-section").find("li").hasClass("error")){
+                $("#phone-section").append("<li class='error'>Invalid phone number.</li>");
+            }
         }
     }
 );
